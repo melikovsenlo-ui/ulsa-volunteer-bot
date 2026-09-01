087b243603be3b229984aa99597b9821d912bb78
melikovsenlo-ui <melikovsenlo@gmail.com>
2026-09-01 15:40:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const INFO_IMAGE =
 // Сюда нужно поставить ПРЯМУЮ ссылку на картинку
 // из этого альбома.
 const RULES_IMAGE =
-  "https://imgur.com/a/00PKkIu#DK3VchY";
+  "ВСТАВЬ_ПРЯМУЮ_ССЫЛКУ_НА_КАРТИНКУ_ПРАВИЛ";
 
 // 🖼️ КАК ВСТУПИТЬ
 const JOIN_IMAGE =
